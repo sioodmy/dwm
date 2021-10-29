7428db3e2bb7a785e03668c30e76c3c3a83013b0
sioodmy <sioodmy@tuta.io>
2021-10-29 23:27:00 +0200

    more fps or sth idk

--- dwm.c
+++ dwm.c
@@ -1590,7 +1590,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 240))
 				continue;
 			lasttime = ev.xmotion.time;
 
